f7e9495d3df1b8a40b054d4f7f40b3105da5ec2c
hgunes <h.gunes13@gmail.com>
2022-11-20 09:04:53 +0100

    init: CryptoApp

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Harun Gunes on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
